84301bdee54a93e7b25acbe5599ee06af0efcb9a
Annoel Skaria <annoel.skaria@betsol.in>
2022-07-01 23:44:35 +0530

    basic layout of api

--- test_api.js
+++ test_api.js
@@ -1,5 +1,12 @@
 const express= require("express");
 const app=express();
+cont pool=require("./db.js");
+app.use(express.json())//=> obj create to access the request from client side
+//routes
+//get data
+//create data
+//update data
+//delete data
 app.listen(3000,()=>{
-    console.log("server is listening to port 3000 ");
+    console.log("server is listening to port 3000  ");
 }) 
