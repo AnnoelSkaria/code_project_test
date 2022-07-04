43673338f99e95ccc38ef672286b7c7a8cd9d9c8
Annoel Skaria <annoel.skaria@betsol.in>
2022-07-04 12:25:24 +0530

    api get user detail done but error

--- test_api.js
+++ test_api.js
@@ -1,12 +1,32 @@
 const express= require("express");
 const app=express();
-cont pool=require("./db.js");
+const pool=require("./db");
 app.use(express.json())//=> obj create to access the request from client side
 //routes
 //get data
-//create data
-//update data
+app.get("/users",async(req,res)=>{
+    try{
+        const allTodos=await pool.query("SELECT*FROM USERS");
+        res.json(allTodos.rows);
+
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+})
+/*create data
+                app.post("/user",async(req,res)=>{
+                    try{
+                        console.log(req.body);
+
+                    }catch
+                    {
+                        console.error(err.message);
+                    }
+            })*/
+            //update data
 //delete data
-app.listen(3000,()=>{
+app.listen(5432,()=>{
     console.log("server is listening to port 3000  ");
 }) 
